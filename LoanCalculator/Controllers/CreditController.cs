8bd9c5a22b35fcd61c24922e18661628b2c41239
Kateryna Tkachenko <katetkacenko@gmail.com>
2020-12-10 12:31:02 +0200

    Initial commit.

---
+++ LoanCalculator/Controllers/CreditController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoanCalculator.Models;
+
+namespace LoanCalculator.Controllers
+{
+    public class CreditController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCredit(Credit credit)
+        {
+            return View(credit);
+        }
+
+        [HttpPost]
+        public IActionResult SetCredit(Credit credit)
+        {
+
+            credit.SetTotalSum();
+
+           return RedirectToAction("GetCredit", credit);
+        }
+    }
+}
---
+++ LoanCalculator/Models/Credit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LoanCalculator.Models
+{
+
+    public class Credit
+    {
+        private const int DaysYear = 365;
+        private const double DaysMonth = 30.5;
+
+        [Required(ErrorMessage = "Credit rate should be set")]
+        public int CreditRate { get; set; }
+        [Required(ErrorMessage = "Credit amount should be set")]
+        [Range(1000, 2000000)]
+        public int CreditAmount { get; set; }
+        [Required(ErrorMessage = "Credit term should be set")]
+        public int CreditTerm { get; set; }
+        public double? TotalSum { get; set; }
+
+        public void SetTotalSum()
+        {
+            var percent =  Math.Round(Convert.ToDouble(CreditAmount) * (Convert.ToDouble(CreditRate) / 100) * Convert.ToDouble((CreditTerm * DaysMonth) / DaysYear));
+            TotalSum = CreditAmount + percent ;
+        }
+    }
+}
